bd14fbbc228c9083002fb43d4320165dbd734a44
C Crow <carrie.g.crow@gmail.com>
2020-05-25 09:56:51 -0500

    Created Nav Component file with react library with placeholder text

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+//Loads the React Library into this component
+import React from 'react';
+
+class Nav extends React.Component {
+	render() {
+		return (
+			<div>
+				Nav Component
+			</div>
+		);
+	}
+}
+
+export default Nav;
