64fddf62d11fe109ca31ac839e97c8b1dd31e545
C Crow <carrie.g.crow@gmail.com>
2020-05-25 10:29:58 -0500

    Added Note to App.js, and linted Note.js and List.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav.js';
 import List from './components/List.js';
+import Note from './components/Note.js';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <List />
+      <Note />
     </div>
   );
 }
