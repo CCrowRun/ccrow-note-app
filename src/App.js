e424b10fad253eee919e6beb77f3c66603289b99
C Crow <carrie.g.crow@gmail.com>
2020-05-25 10:22:09 -0500

    Updated navbar logo and styling. Added List component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav.js';
+import List from './components/List.js';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <List />
     </div>
   );
 }
