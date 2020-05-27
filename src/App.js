07ee4a4999edb43e95eeab0862f959898726cd8e
C Crow <carrie.g.crow@gmail.com>
2020-05-27 13:05:30 -0500

    Updated App.js to use a constructor and set a state key of showNote, then referenced state key for render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav.js';
 import List from './components/List.js';
 import Note from './components/Note.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Nav />
-      <List />
-      <Note />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
+  render() {
+    const { showNote } = this.state;
+
+    return (
+      <div className="App">
+        <Nav />
+        { showNote ? <Note /> : <List /> }
+      </div>
+    );
+  }
 }
 
 export default App;
