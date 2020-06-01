09d647f6f54ac7e3eef9cce3ff23ce7f3f487ea1
C Crow <carrie.g.crow@gmail.com>
2020-06-01 11:17:07 -0500

    Dynamically populates the NoteCard with the properties (title, content) from API

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 class NoteCard extends React.Component {
 	render() {
+		const { note } = this.props;
+
 		return(
 			<div className="note-card-container">
-				NoteCard Component
+				<div className="note-card-title">
+					{note.title}
+				</div>
+				<div className="note-card-content">
+					{note.content}
+				</div>
 			</div>
 		);
 	}
