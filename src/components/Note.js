e5b5abdfdbcba937314f485aac7301e62805645e
C Crow <carrie.g.crow@gmail.com>
2020-06-02 10:01:25 -0500

    Created onSubmit function, and added ref to the note's title field

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 class Note extends React.Component {
+	onSubmit(e) {
+		e.preventDefault();
+		const title = this.title.value;
+		console.log(title);
+	}
+
 	render() {
 		const { note } = this.props;
 
 		return (
 			<div className="note-container">
-				<form className="note-form">
+				<form className="note-form" onSubmit={(e) => this.onSubmit(e)}>
 					<input 
 						className="note-title-input" 
 						type="text" 
 						placeholder="Note Title..." 
 						defaultValue={note.title} 
+						ref={(input) => this.title = input}
 					/>
 					<textarea 
 						className="note-textarea" 
