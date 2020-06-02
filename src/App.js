5ffce74908e73994e9a52327e636327287f28b7d
C Crow <carrie.g.crow@gmail.com>
2020-06-02 10:32:14 -0500

    extracted axios request to a new function, use performSubmissionRequest to determine if an id is present, and returns the patch or post request

--- src/App.js
+++ src/App.js
@@ -35,8 +35,16 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }
 
-  submitNote = (data) => {
-    axios.post(urlFor('notes'), data)
+  performSubmissionRequest = (data, id) => {
+    if (id) {
+      return axios.patch(urlFor(`notes/${id}`), data);
+    } else {
+      return axios.post(urlFor('notes'), data);
+    }
+  }
+
+  submitNote = (data, id) => {
+    this.performSubmissionRequest(data, id)
       .then((res) => this.setState({ showNote: false }) )
       .catch((err) => console.log(err) );
   }
