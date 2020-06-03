e266eb385cf47c1746c31f0d2dad99a632f8e25a
C Crow <carrie.g.crow@gmail.com>
2020-06-03 10:15:53 -0500

    Created tag form

--- src/components/Note.js
+++ src/components/Note.js
@@ -23,6 +23,16 @@ class Note extends React.Component {
 					</i>
 				</span>
 			);
+		} else {
+			return (
+				<form>
+					<input 
+						className="tag-input" 
+						type="text" 
+						placeholder="Tag Name..."
+					/>
+				</form>
+			);
 		}
 
 	}
